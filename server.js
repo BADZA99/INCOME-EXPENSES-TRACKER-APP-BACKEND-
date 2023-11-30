34b555d76641fe63fcab38187b0565d5946b55d3
papabn <pndiaye999@gmail.com>
2023-11-30 02:26:42 +0000

    Add account and transaction routes

--- server.js
+++ server.js
@@ -54,8 +54,90 @@ app.put('/api/v1/users/:id',async(req,res)=>{
 
 // account routes
 
+// POST/api/v1/accounts
+app.post('/api/v1/accounts',async(req,res)=>{
+    try {
+        res.json({msg:'Create account route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
+// Get/api/v1/accounts/:id
+app.get('/api/v1/accounts/:id',async(req,res)=>{
+    try {
+        res.json({msg:'Get single Account route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
+// delete/api/v1/accounts/:id
+app.delete('/api/v1/accounts/:id',async(req,res)=>{
+    try {
+        res.json({msg:'delete Account route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
+// PUT/api/v1/accounts/:id
+app.put('/api/v1/accounts/:id',async(req,res)=>{
+    try {
+        res.json({msg:'update Account route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
 
 // transaction route
+// post /api/v1/transactions
+app.post('/api/v1/transactions',async(req,res)=>{
+    try {
+        res.json({msg:'Create transaction route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
+// Get/api/v1/transactions
+app.get('/api/v1/transactions',async(req,res)=>{
+    try {
+        res.json({msg:'Get transactions route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
+// Get/api/v1/transactions/:id
+app.get('/api/v1/transactions/:id',async(req,res)=>{
+    try {
+        res.json({msg:'Get single transaction route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
+// delete/api/v1/transactions/:id
+app.delete('/api/v1/transactions/:id',async(req,res)=>{
+    try {
+        res.json({msg:'delete transaction route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
+// PUT/api/v1/transactions/:id
+app.put('/api/v1/transactions/:id',async(req,res)=>{
+    try {
+        res.json({msg:'update transaction route'})
+    } catch (error) {
+       res.json(error) ;
+    }
+})
+
+
 
 
 // error handlers
